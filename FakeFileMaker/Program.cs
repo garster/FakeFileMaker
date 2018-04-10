b2a1a2603f666c88e54a9152f9af6d2f087c90ec
Gary Scott <gary.scott@ucsb.edu>
2018-04-10 11:52:13 -0700

    Commit program files.

---
+++ FakeFileMaker/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FakeFileMaker
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      const int x = 1024;
+
+      for (var kb = 1; kb < 11; kb++)
+      {
+        var b = kb * x;
+
+        var procStartInfo =
+          new System.Diagnostics.ProcessStartInfo("cmd",
+            $"/c fsutil file createnew FakeFile({kb} KB).ext {b}")
+          {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+          };
+
+        var proc = new System.Diagnostics.Process { StartInfo = procStartInfo };
+        proc.Start();
+      }
+    }
+  }
+}
